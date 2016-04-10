baedad0a29f4e008109127550b89caaaa09fd127
Ryan Niehaus <ryan.niehaus@gmail.com>
2016-04-09 19:34:30 -0500

    buildpack update

--- .profile.d/heroku-bash-buildpack-defaults.sh
+++ .profile.d/heroku-bash-buildpack-defaults.sh
@@ -3,5 +3,6 @@
 echo "-----> Executing .profile.d/heroku-bash-buildpack-defaults.sh"
 #extra buildpack configuration goes here
 export myTESTDIR_INBUILDPACK_DEFAULTS=BLAH
+export PATH="/app/bin:$PATH"
 echo "-----> FINISHED Executing .profile.d/heroku-bash-buildpack-defaults.sh"
 
